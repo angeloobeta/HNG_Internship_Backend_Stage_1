f20ba0e01997f897356d63987b4b464573b3e988
ifeanyichukwuobeta <angeloobeta@hotmail.com>
2023-09-10 17:16:42 +0100

    Added: Added Dockerfile with configuration

--- src/main/java/com/example/hng_internship_backendstage_1/entity/ApiResponse.java
+++ src/main/java/com/example/hng_internship_backendstage_1/entity/ApiResponse.java
@@ -12,10 +12,9 @@ import java.time.format.DateTimeFormatter;
     private String current_day = LocalDateTime.now().getDayOfWeek().toString().charAt(0) + LocalDateTime.now().getDayOfWeek().toString().substring(1).toLowerCase();;
     private String utc_time = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'"));
     private String track;
-    private String github_file_url = "https://github.com/angeloobeta/HNG Internship Backend Stage1/file";
-    private String github_repo_url = "https://github.com/angeloobeta/HNG Internship Backend Stage1";
+    private String github_file_url = "https://github.com/angeloobeta/HNG_Internship_Backend_Stage_1/blob/master/src/main/java/com/example/hng_internship_backendstage_1/HngInternshipBackendStage1Application.java";
+    private String github_repo_url = "https://github.com/angeloobeta/HNG_Internship_Backend_Stage_1";
     private int status_code = 200;
 
-    // Constructors, getters, and setters
 }
 
